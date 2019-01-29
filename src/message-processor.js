48f598d5490548b0b6bf55ef146bd9d59eae5398
Ian Fisk <ian.fisk@faithlife.com>
2019-01-29 09:41:37 -0800

    Create file message response.

--- src/message-processor.js
+++ src/message-processor.js
@@ -1,8 +1,13 @@
 'use strict';
 
+const fs = require('fs');
+const util = require('util');
+const path = require('path');
 const Flowdock = require('flowdock');
 const config = require('./config');
 const { WorkerBase } = require('./worker-pool');
+
+const readFile = util.promisify(fs.readFile);
 
 class MessageProcessor extends WorkerBase {
 	constructor() {
@@ -22,12 +27,27 @@ class MessageProcessor extends WorkerBase {
 			return;
 		}
 
+		await this.uploadFile(workerData);
+	}
+
+	async uploadFile({ flowId, threadId }) {
+		const fileContents = await readFile(path.join(__dirname, 'me.jpeg'));
+		const buffer = Buffer.from(fileContents);
+
 		await new Promise((resolve, reject) => {
-			this.session.threadMessage(
-				flowId,
-				threadId,
-				`Hello from the fiskars bot! Your query is ${queryText}`,
-				null,
+			this.session.post(
+				'/messages',
+				{
+					event: 'file',
+					flow: flowId,
+					thread_id: threadId,
+					tags: [],
+					content: {
+						data: buffer.toString('base64'),
+						content_type: 'image/png',
+						file_name: 'cabinet_icon.png',
+					},
+				},
 				err => {
 					if (!err) {
 						resolve();
